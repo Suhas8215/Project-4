e6d56ffff2bac65186083f94b75256460712fd5d
DasSkippy <sean.frederico105@gmail.com>
2025-12-03 12:11:55 -0800

    started working on main area

--- src/scenes/MainArea.js
+++ src/scenes/MainArea.js
@@ -6,10 +6,17 @@ export class MainArea extends Phaser.Scene {
 
     preload() {
         //this.preloadConfig();
+        this.load.tilemapTiledJSON('MainArea', 'assets/tilemaps/MainArea.tmj');
+        this.load.image('TownTileset', 'assets/tilemap/tilemap_packed.png');
     }
 
     create() {
+        const map = this.add.tilemap("MainArea");
+        const tiles = map.addTilesetImage("tiles", "TownTileset");
 
+        const backgroundLayer = map.createLayer("Background", tiles);
+        const decorationLayer = map.createLayer("Decoration", tiles);
+        const terrainLayer = map.createLayer("Terrain", tiles);
     }
 
 }
